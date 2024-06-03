78b33d983c0be8173d6020194d09b8005f8e2b43
knockknock <yangban11111@gmail.com>
2024-06-03 16:51:12 +0900

    Refactor: enum 활용

--- TableViewProject/SettingsTableViewController.swift
+++ TableViewProject/SettingsTableViewController.swift
@@ -7,49 +7,65 @@
 
 import UIKit
 
+enum SettingOptions: CaseIterable {
+    case total, personal, others
+    
+    var mainOptions: String {
+        switch self {
+        case .total:
+            return "전체 설정"
+        case .personal:
+            return "개인 설정"
+        case .others:
+            return "기타"
+        }
+    }
+    
+    var subOptions: [String] {
+        switch self {
+        case .total:
+            return ["공지사항", "실험실", "버전 정보"]
+        case .personal:
+            return ["개인/보안", "알림", "채팅", "멀티프로필"]
+        case .others:
+            return ["고객센터/도움말"]
+        }
+    }
+}
+//var settings = SettingOptions.allCases.count
+
 class SettingsTableViewController: UITableViewController {
     
-    let sections = ["전체 설정", "개인 설정", "기타"]
-    let entireItems = ["공지사항", "실험실", "버전 정보"]
-    let privateItems = ["개인/보안", "알림", "채팅", "멀티프로필"]
-    let etcItems = ["고객센터/도움말"]
+    let allcases = SettingOptions.allCases
     
     override func viewDidLoad() {
         super.viewDidLoad()
+//        print(allcases)
     }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return sections[section]
+        return allcases[section].mainOptions
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return sections.count
+        return allcases.count
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if section == 0 {
-            return entireItems.count
-        } else if section == 1 {
-            return privateItems.count
-        } else if section == 2 {
-            return etcItems.count
-        } else {
-            return 0
-        }
+        return allcases[section].subOptions.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "setting1", for: indexPath)
         
         cell.textLabel?.font = .systemFont(ofSize: 13)
-        
-        if indexPath.section == 0 {
-            cell.textLabel?.text = entireItems[indexPath.row]
-        } else if indexPath.section == 1 {
-            cell.textLabel?.text = privateItems[indexPath.row]
-        } else {
-            cell.textLabel?.text = etcItems[indexPath.row]
-        }
+        let section = indexPath.section
+        let row = indexPath.row
+        cell.textLabel?.text = allcases[section].subOptions[row]
         
         return cell
     }
